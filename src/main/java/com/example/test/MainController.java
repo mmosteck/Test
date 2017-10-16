1c5b765c4c8d4bf805e906bb3948485cb74e6179
mmosteck <mmosteck@gmail.com>
2017-10-16 22:23:24 +0200

    init commit

---
+++ src/main/java/com/example/test/MainController.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class MainController
+{
+    @RequestMapping("/")
+    String index(Model model)
+    {
+        System.out.println("Keks");
+        return "index.html";
+    }
+}
